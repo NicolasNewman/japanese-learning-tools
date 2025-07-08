f870ea5bc266894a2035518036cc87b3ebd29538
NicolasNewman <nanewman01@gmail.com>
2025-07-07 19:35:50 -0500

    feat(jp-learning-tools): added temp logs to debug menu

--- apps/jp-learning-tools/src/lib/commands.ts
+++ apps/jp-learning-tools/src/lib/commands.ts
@@ -3,3 +3,4 @@ import { invoke } from '@tauri-apps/api/core';
 
 export const externalBinaryDir = async () => await invoke<string>('external_binary_dir');
 export const openDevTools = async () => await invoke<string>('open_devtools');
+export const openTmpLog = async () => await invoke<string>('open_tmp_log');
