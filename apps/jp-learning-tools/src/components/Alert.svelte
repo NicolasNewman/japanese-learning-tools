0a5170e73cf4fdb202c27d476fd81c762e8e3281
NicolasNewman <nanewman01@gmail.com>
2025-09-05 14:29:29 -0500

    chore(jp-learning-tools): increased alert top spacing

--- apps/jp-learning-tools/src/components/Alert.svelte
+++ apps/jp-learning-tools/src/components/Alert.svelte
@@ -20,7 +20,7 @@
 </script>
 
 {#if alertState.alert !== null}
-  <div class="absolute top-0 left-0 w-full flex justify-center">
+  <div class="absolute top-2 left-0 w-full flex justify-center">
     <Alert.Root class="w-fit min-w-1/2">
       <CircleAlertIcon class="size-4" />
       <Alert.Title>{alertState.alert.alertTitle}</Alert.Title>
